2ec93f1976f61543b22bd55ec71e262e51044189
Nevsimao <coday.ink@gmail.com>
2021-04-22 14:57:46 -0400

    uniquely identify the current logged in user

--- the-talk/src/views/Talk.vue
+++ the-talk/src/views/Talk.vue
@@ -71,3 +71,49 @@
     </div>
   </div>
 </template>
+
+<script>
+import { CometChat } from "@cometchat-pro/chat";
+import NavBar from "../components/NavBar.vue";
+import Spinner from "../components/Spinner.vue";
+
+export default {
+  name: "home",
+  components: {
+    NavBar,
+    Spinner
+ },
+  data() {
+    return {
+      username: "",
+      avatar: "",
+      uid: "",
+      sendingMessage: false,
+      chatMessage: "",
+      loggingOut: false,
+      groupMessages: [],
+      loadingMessages: false
+    };
+  },
+  created() {
+    this.getLoggedInUser();
+  },
+  methods: {
+    getLoggedInUser() {
+      CometChat.getLoggedinUser().then(
+        user => {
+          this.username = user.name;
+          this.avatar = user.avatar;
+          this.uid = user.uid;
+        },
+        error => {
+          this.$router.push({
+            name: "homepage"
+          });
+          console.log(error);
+        }
+      );
+    },
+  }
+};
+</script>
